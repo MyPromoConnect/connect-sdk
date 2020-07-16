e6d0b5c898ad73847b5f5fe9ab794c238fe5e3e6
Massimo Minutillo <mmi@os-cillation.de>
2020-07-16 14:47:04 +0200

    added classes for job routes

---
+++ src/Exceptions/JobException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: massimo
+ * Date: 16.07.20
+ * Time: 14:42
+ */
+
+namespace MyPromo\Connect\SDK\Exceptions;
+
+use Exception;
+
+/**
+ * Class JobException
+ * @package MyPromo\Connect\SDK\Exceptions
+ */
+class JobException extends Exception
+{
+}
--- src/Repositories/Jobs/JobRepository.php
+++ src/Repositories/Jobs/JobRepository.php
@@ -8,7 +8,58 @@
 
 namespace MyPromo\Connect\SDK\Repositories\Jobs;
 
-class JobRepository
-{
+use MyPromo\Connect\SDK\Exceptions\ClientException;
+use MyPromo\Connect\SDK\Exceptions\JobException;
+use MyPromo\Connect\SDK\Exceptions\MissingCredentialsException;
+use MyPromo\Connect\SDK\Repositories\Repository;
 
+class JobRepository extends Repository
+{
+    /**
+     * @return array
+     * @throws JobException
+     * @throws ClientException
+     * @throws MissingCredentialsException
+     */
+    public function all()
+    {
+        $response = $this->client->guzzle()->get('/v1/jobs', [
+            'headers' => [
+                'Accept'        => 'application/json',
+                'Authorization' => 'Bearer ' . $this->client->auth()->get(),
+            ]
+        ])
+        ;
+
+        if ($response->getStatusCode() !== 200) {
+            throw new JobException($response->getBody(), $response->getStatusCode());
+        }
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * @param $jobId
+     *
+     * @return array
+     * @throws JobException
+     * @throws ClientException
+     * @throws MissingCredentialsException
+     */
+    public function find($jobId)
+    {
+        $response = $this->client->guzzle()->get('/v1/jobs/' . $jobId, [
+            'headers' => [
+                'Accept'        => 'application/json',
+                'Authorization' => 'Bearer ' . $this->client->auth()->get(),
+            ],
+        ])
+        ;
+
+        if ($response->getStatusCode() !== 200) {
+            throw new JobException($response->getBody(), $response->getStatusCode());
+        }
+
+        return json_decode($response->getBody(), true);
+    }
 }
