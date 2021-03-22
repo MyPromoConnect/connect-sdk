a2bb7d9a93116934b2593606657254e914b38263
mypromo-ml <64786812+mypromo-ml@users.noreply.github.com>
2021-03-22 14:29:17 +0100

    Ml/feature/php 73 update (#35)
    * update PHP Version and Guzzle Package
    
    * update composer files
    
    * update travis php version to 7.3
    
    * update readme as well
    
    * spelling issue

--- src/Exceptions/MissingOrderException.php
+++ src/Exceptions/MissingOrderException.php
@@ -8,7 +8,7 @@ use Exception;
  * Class MissingOrderException
  * @package Connect\SDK\Exceptions
  *          Exception when order id is missing
- *          e.g. in @see \Connect\SDK\Models\OrderItem
+ *          e.g. in @see \MyPromo\Connect\SDK\Models\OrderItem
  */
 class MissingOrderException extends Exception
 {
