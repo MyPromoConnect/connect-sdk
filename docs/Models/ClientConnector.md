23717d0495d736551ad58ec2a7cf2f3d7a30f0a1
ali <ua@mypromo-service.com>
2022-02-16 16:41:48 +0500

    client connector routes are done

---
+++ docs/Models/ClientConnector.md
@@ -0,0 +1,19 @@
+Template-File for [ClientConnectorRepository->create()][ClientConnectorRepository]
+
+```php
+$clientConnector = new \MyPromo\Connect\SDK\Models\ClientConnector();
+
+$clientConnector->setConnectorId(14);
+$clientConnector->setConnectorKey('shopify');
+$clientConnector->setTarget('sales_channel');
+
+# for magento connector
+$magentoConfigurations = new \MyPromo\Connect\SDK\Helpers\MagentoConnectorConfigurations();
+$clientConnector->setConfiguration($magentoConfigurations);
+
+# for shopify connector
+$shopifyConfigurations = new \MyPromo\Connect\SDK\Helpers\ShopifyConnectorConfigurations();
+$clientConnector->setConfiguration($shopifyConfigurations);
+```
+
+[ClientConnectorRepository]: ../Repositories/ClientConnectorRepository.md
---
+++ src/Repositories/Client/ClientConnectorRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace MyPromo\Connect\SDK\Repositories\Client;
+
+use Exception;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\RequestOptions;
+use MyPromo\Connect\SDK\Exceptions\ClientConnectorException;
+use MyPromo\Connect\SDK\Models\ClientConnector;
+use MyPromo\Connect\SDK\Repositories\Repository;
+use Psr\Cache\InvalidArgumentException;
+
+class ClientConnectorRepository extends Repository
+{
+    /**
+     * Update client connectors
+     *
+     * @param ClientConnector $clientConnector
+     * @return mixed
+     *
+     * @throws ClientConnectorException
+     * @throws GuzzleException
+     * @throws InvalidArgumentException
+     */
+    public function update(ClientConnector $clientConnector)
+    {
+        try {
+            $response = $this->client->guzzle()->patch('/v1/client/connectors', [
+                'headers'           => [
+                    'Accept'        => 'application/json',
+                    'Content-Type'  => 'application/json',
+                    'Authorization' => 'Bearer ' . $this->client->auth()->get(),
+                ],
+                RequestOptions::JSON => $clientConnector->toArray(),
+            ]);
+
+            if ($response->getStatusCode() !== 201) {
+                throw new ClientConnectorException($response->getBody(), $response->getStatusCode());
+            }
+
+            $body = json_decode($response->getBody(), true);
+
+        } catch (Exception $ex) {
+            throw new ClientConnectorException($ex->getMessage(), $ex->getCode());
+        }
+
+        return $body;
+    }
+}
