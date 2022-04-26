7a617adc01d8b82392ae5448dea800647a8baebd
Umair Ali <46967039+umairtanveer@users.noreply.github.com>
2022-04-26 15:42:00 +0200

    Invalid response exception

---
+++ src/Exceptions/InvalidResponseException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MyPromo\Connect\SDK\Exceptions;
+
+use Exception;
+
+/**
+ * Class InvalidResponseException
+ * @package MyPromo\Connect\SDK\Exceptions
+ * Thrown when something went wrong in the design process
+ */
+class InvalidResponseException extends Exception
+{}
