2e262bf7db2baed259eb1d083f5971a94673ffae
Raymund Fischer <raymundfischer@Raymunds-MacBook-Pro-M1.local>
2022-04-26 14:45:40 +0200

    improve exception handles

--- src/Exceptions/ProductExportException.php
+++ src/Exceptions/ProductExportException.php
@@ -3,6 +3,7 @@
 namespace MyPromo\Connect\SDK\Exceptions;
 
 use Exception;
+use Throwable;
 
 /**
  * Class ProductExportException
@@ -11,4 +12,52 @@ use Exception;
  */
 class ProductExportException extends Exception
 {
+    /**
+     * @var array
+     */
+    protected $responseBody;
+
+    /**
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->responseBody = json_decode($previous->getMessage());
+    }
+
+    /**
+     * @return array
+     */
+    public function getApiResponse(): array
+    {
+        return $this->responseBody;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiResultSuccess(): string
+    {
+        return $this->responseBody->success;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiResultMessage(): string
+    {
+        return $this->responseBody->message;
+    }
+
+    /**
+     * @return array
+     */
+    public function getApiResultErrors(): array
+    {
+        return $this->responseBody->errors;
+    }
 }
